8c2b0634f52affdf341536cfdb80c9549a45267d
Luis Salazar <bp.lusv@gmail.com>
2020-10-23 11:47:40 -0600

    Task2 + analysis completed

--- Task2.py
+++ Task2.py
@@ -20,3 +20,16 @@ Print a message:
 September 2016.".
 """
 
+total_time = {}
+
+for call in calls:
+    incoming_number, answering_number, duration = call[0], call[1], int(call[3])
+    total_time[incoming_number] = total_time.get(incoming_number, 0) + duration
+    total_time[answering_number] = total_time.get(answering_number, 0) + duration
+
+max_number = calls[0][0]
+for number in total_time:
+    if total_time[number] > total_time[max_number]:
+        max_number = number
+
+print(f'{max_number} spent the longest time, {total_time[max_number]} seconds, on the phone during September 2016.')
