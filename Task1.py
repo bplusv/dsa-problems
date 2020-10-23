a87b199cdf5af407891e2801ac0daa6ca9d707b4
Luis Salazar <bp.lusv@gmail.com>
2020-10-23 10:15:29 -0600

    Task1 + analysis completed

--- Task1.py
+++ Task1.py
@@ -19,3 +19,18 @@ Print a message:
 "There are <count> different telephone numbers in the records."
 """
 
+unique_numbers = set()
+
+for text in texts:
+    calling_number, answering_number = text[0], text[1]
+    unique_numbers.add(calling_number)
+    unique_numbers.add(answering_number)
+
+for call in calls:
+    calling_number, answering_number = call[0], call[1]
+    unique_numbers.add(calling_number)
+    unique_numbers.add(answering_number)
+
+count = len(unique_numbers)
+
+print(f'There are {count} different telephone numbers in the records.')
